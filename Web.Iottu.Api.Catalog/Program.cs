fc27666cf469dfb539c8b42f8a79121d99e09225
levmn <levimncontato@gmail.com>
2025-09-29 11:15:44 -0300

    adding database credentials manager

--- Web.Iottu.Api.Catalog/Program.cs
+++ Web.Iottu.Api.Catalog/Program.cs
@@ -2,11 +2,17 @@ using Infrastructure.Iottu.Persistence.Contexts;
 using Infrastructure.Iottu.Persistence.Repositories;
 using Core.Iottu.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Connection String
-var connectionString = builder.Configuration.GetConnectionString("dbIottu");
+Env.Load();
+
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var baseConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var connectionString = $"User Id={dbUser};Password={dbPassword};{baseConnection}";
 
 builder.Services.AddDbContext<IottuDbContext>(options =>
     options.UseOracle(connectionString));
